8da9b99ba0fd27de95ca8a2fcc892afb2dac8a42
Edwin Harmata <edwin.harmata@gmail.com>
2020-02-14 21:36:40 +0100

    First step in rewriting html template into the php

---
+++ lovetoeat/wp-content/themes/LoveToEat/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+if(!defined('LOVETOEAT_THEME_DIR')){
+    define('LOVETOEAT_THEME_DIR', get_theme_root().'/'.get_template().'/');
+}
+
+if(!defined('LOVETOEAT_THEME_URL')){
+    define('LOVETOEAT_THEME_URL', WP_CONTENT_URL.'/'.'themes/'.get_template().'/');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
